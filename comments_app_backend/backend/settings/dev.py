0437174b11688ba839017a7fcb2986fa956bb052
arturboyun <arturboyun@gmail.com>
2020-10-20 21:03:46 +0300

    Change database to MySQL

--- comments_app_backend/backend/settings/dev.py
+++ comments_app_backend/backend/settings/dev.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'comments_app',
+        'USER': 'comments_app_user',
+        'PASSWORD': 'comments_app_super_secret_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
